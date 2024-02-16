bb13f7017453db7d222686857dd5c813f9d077fa
Sai Maneesh <saimaneesh1115@gmail.com>
2024-02-16 12:47:25 +0530

    iginiting the app

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("h1", {}, "i am h1 tag"),
   React.createElement("h2", {}, "i am h2 tag"),
